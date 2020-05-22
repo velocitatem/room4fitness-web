5641afbf1dde47f96bd1a2e769078b76e6750b20
Daniel Rosel <danalves24@outlook.com>
2020-05-22 23:26:55 +0200

    Frontend - Homepage made

--- src/App.js
+++ src/App.js
@@ -18,21 +18,34 @@ function main() {
 
 function App() {
   return (
-    <div class="container">
+    <div class="container" id='homeContainer'>
       <Nav></Nav>
       <div class="">
         <div class="container">
           <div class="row">
-            <div class="col-lg-12">
-              <center>
-                <h1>room4fitness</h1>
-              </center>
+            <div class="col-lg-12" id='cover'>
             </div>
           </div>
+          <div class="row" id="initalDescHome">
+            <div class="col-sm-4" id="essQ"><h2>Dont Quite Understand An Exercise From a Video?</h2></div>
+            <div class="col-sm-4" id="essQ"><h2>Do You Want a Better Explanation Of an Exercise?</h2></div>
+            <div class="col-sm-4" id="essQ"><h2>Growing Sick of Basic Exercise Videos?</h2></div>
+          </div>
+          <br></br>
+          <hr></hr>
+          <br></br>
           <div class="row">
-            <div class="col-sm-8">
+            <div class="col-lg-12">
+              <h1 id="essQ">Use room4fitness!</h1>
+              <h4 id="essQ">
+                Get in-depth explanations of exercises with video and audio guidence
+              </h4>                 
             </div>
-            <div class="col-sm-4">
+
+          </div>
+          <div class="row">
+            <div class="col-lg-12">
+              <h2 id="essQ"><a href="/trainings">Start Now</a></h2>
             </div>
           </div>
         </div>
