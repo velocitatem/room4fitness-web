cd1a733e3f7e11aff26a7914c6599d06e2091ce7
Daniel Rosel <danalves24@outlook.com>
2020-05-22 19:15:27 +0200

    11750

--- src/components/nav.js
+++ src/components/nav.js
@@ -18,13 +18,10 @@ function App() {
   return (
 <div className="App">
       <header class="App-header">
-      <nav class="navbar navbar-expand-sm " id="navbarM">
+      <nav class="navbar navbar-expand-sm navbar-dark bg-dark" id="navbarM">
       <div class="" id="navbarTogglerDemo01">      
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-        <a class="navbar-brand" href="/">Home</a>
-          <li class="nav-item">
-            <a class="nav-link" href="/test" id="navI"><b>Demo</b></a>            
-          </li>              
+        <a class="navbar-brand" href="/">Home</a>           
           <li class="nav-item">
             <a class="nav-link" href="/trainings" id="navI">Trainings</a>            
           </li>              
